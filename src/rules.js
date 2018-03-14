25a7ae22d9d9507d2c3cb22a56b3fc041d995c68
Birkenstab <git@birkenstab.de>
2018-03-14 22:59:13 +0100

    Rules: RNET Labor- und Vorlesungstermine eingetragen

--- src/rules.js
+++ src/rules.js
@@ -57,5 +57,33 @@ module.exports = [
       lecture.name += " Tutorium"
       lecture.shortName += "-Tut."
     }
+  },
+  { // RNET
+    moduleNo: "043020271000",
+    day: 4,
+    action(lecture) {
+      console.log(lecture)
+      if (!_.some([{ hour: 11, minute: 30 }, { hour: 14, minute: 0 }], lecture.timeFrom)
+        || !_.some([{ hour: 13, minute: 0 }, { hour: 15, minute: 30 }], lecture.timeTo))
+        return
+
+      console.log("weiter")
+
+      const lab = [
+        { day: 29, month: 3, year: 2018 },
+        { day: 5, month: 4, year: 2018 },
+        { day: 26, month: 4, year: 2018 },
+        { day: 3, month: 5, year: 2018 },
+        { day: 7, month: 6, year: 2018 },
+        { day: 14, month: 6, year: 2018 }
+      ]
+      if (lecture.room === "C12 - Labor Rechnernetze") {
+        console.log("Filtere Labs")
+        lecture.dates = lecture.dates.filter(date => _.some(lab, date))
+      } else if (lecture.room === "A103 - Hörsaal A103") {
+        console.log("Filtere Vorlesung")
+        lecture.dates = lecture.dates.filter(date => !_.some(lab, date))
+      }
+    }
   }
 ]
